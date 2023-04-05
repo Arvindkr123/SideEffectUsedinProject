33ce6ccdaae2e9d7ffa7506b205d05676d5503d6
Arvind <thakurarvindkr10@gmail.com>
2023-04-05 16:00:08 +0530

    that's the side effects problem

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = localStorage.getItem('setLoggedIn');
+  if(login==='1'){
+    setIsLoggedIn(true);
+  }
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('setLoggedIn','1');
     setIsLoggedIn(true);
   };
 
