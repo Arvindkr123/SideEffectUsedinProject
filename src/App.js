4a25a824542e42f04dee5e454077406f0e9158f1
Arvind <thakurarvindkr10@gmail.com>
2023-04-08 20:58:50 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../src/Styles/App.scss'
+import '../src/Styles/header.scss'
+import '../src/Styles/home.scss'
+import '../src/Styles/footer.scss'
+import '../src/Styles/contact.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Services from './components/Services.jsx';
+
+const App = () => {
+    return (
+        <>
+        <Router>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+                <Route path='/services' element={<Services/>}/>
+            </Routes>
+            <Footer/>
+        </Router>
+        </>
+    )
+}
+
+export default App;
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.goole.com" target={'blank'}>Google</a>
+            <a href="https://www.linkdin.com" target={'blank'}>Linkdin</a>
+            <a href="https://www.twitter.com" target={'blank'}>Twitter</a>
+            <a href="https://www.arvindkr@123.com" target={'blank'}>GitHub</a>
+            <a href="https://www.youtube.com" target={'blank'}>YouTube</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
