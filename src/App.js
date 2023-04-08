f6164eb37a5b5ef9d3de0d5f87e7e350470ff6ad
Arvind <thakurarvindkr10@gmail.com>
2023-04-08 15:06:52 +0530

    modify

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Style/App.css';
+import Header from './components/Header';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Product from './components/Product';
+
+
+
+const App = () => {
+    return (
+        <>
+        <Router>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/product/:id" element={<Product/>}/>
+                <Route path="*" element={<div><h1>Page not found 404</h1></div>}/>
+            </Routes>
+        </Router>
+        </>
+    )
+}
+
+export default App
